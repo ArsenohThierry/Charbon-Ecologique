75352d74412859bd09046c0dbb00b66ad982cd54
NRMiaro <miarotools@gmail.com>
2026-06-28 16:46:39 +0300

    ajout model+repo pour produit

---
+++ src/main/java/com/example/charbonecolo/model/ProduitModel.java
@@ -0,0 +1,41 @@
+package com.example.charbonecolo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produit")
+public class ProduitModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nom;
+
+    private Double pu;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Double getPu() {
+        return pu;
+    }
+
+    public void setPu(Double pu) {
+        this.pu = pu;
+    }
+
+}
