ee939b076e3b23350609e6a369bfaee4b08ccf77
faniry74 <faniryanthony94@gmail.com>
2026-06-28 19:23:56 +0300

    Revert "Feature/production"

--- src/main/java/com/example/charbonecolo/service/FournisseurService.java
+++ src/main/java/com/example/charbonecolo/service/FournisseurService.java
@@ -1,30 +1,8 @@
 package com.example.charbonecolo.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.charbonecolo.model.FournisseurModel;
-import com.example.charbonecolo.repository.FournisseurRepository;
 
 @Service
 public class FournisseurService {
 
-    @Autowired
-    private FournisseurRepository fournisseurRepository;
-
-    public List<FournisseurModel> getAll(){
-        return fournisseurRepository.findAll();
-    }
-
-    @Transactional
-    public void persistFournisseur(FournisseurModel fournisseurModel){
-        fournisseurRepository.save(fournisseurModel);
-    }
-
-    public FournisseurModel getById(Integer id){
-        return fournisseurRepository.findById(id).get();
-    }
 }
--- src/main/java/com/example/charbonecolo/service/TypeMatierePremiereService.java
+++ src/main/java/com/example/charbonecolo/service/TypeMatierePremiereService.java
@@ -1,13 +1,8 @@
 package com.example.charbonecolo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.charbonecolo.repository.TypeMatierePremiereRepository;
 
 @Service
 public class TypeMatierePremiereService {
 
-    @Autowired
-    private TypeMatierePremiereRepository typeMatierePremiereRepository;
 }
