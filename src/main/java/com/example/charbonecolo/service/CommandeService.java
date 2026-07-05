dd3df9b8c9a5445ba77c431a36f20a1d81a290ac
Nomena-coder <raobelinanomenjanahary@gmail.com>
2026-07-05 08:37:54 +0300

    Insertion + annulation de commande -> Stock

--- src/main/java/com/example/charbonecolo/service/CommandeService.java
+++ src/main/java/com/example/charbonecolo/service/CommandeService.java
@@ -1,6 +1,7 @@
 package com.example.charbonecolo.service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,16 +20,27 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.charbonecolo.dto.CommandeDto;
 import com.example.charbonecolo.dto.CriteriaWrapper;
+import com.example.charbonecolo.dto.EntreeStockDTO;
 import com.example.charbonecolo.dto.SessionDetailErrorWrapper;
+import com.example.charbonecolo.dto.SortieStockDTO;
 import com.example.charbonecolo.exception.InvalidCommandeException;
 import com.example.charbonecolo.exception.StockUnavailableException;
 import com.example.charbonecolo.model.CommandeModel;
 import com.example.charbonecolo.model.CommandeStatutModel;
 import com.example.charbonecolo.model.DetailCommandeModel;
+import com.example.charbonecolo.model.LotProductionModel;
+import com.example.charbonecolo.model.LotStatutsModel;
+import com.example.charbonecolo.model.MouvementSortieDetailModel;
+import com.example.charbonecolo.model.MouvementStockModel;
 import com.example.charbonecolo.model.StatutCommandeModel;
+import com.example.charbonecolo.model.StatutsLotProductionModel;
+import com.example.charbonecolo.model.TypeMouvementStockModel;
 import com.example.charbonecolo.repository.CommandeRepository;
 import com.example.charbonecolo.repository.DetailCommandeRepository;
+import com.example.charbonecolo.repository.LotProductionRepository;
+import com.example.charbonecolo.repository.MouvementSortieDetailRepository;
 import com.example.charbonecolo.repository.StatutCommandeRepository;
+import com.example.charbonecolo.repository.StatutsLotProductionRepository;
 
 @Service
 public class CommandeService {
@@ -38,14 +50,20 @@ public class CommandeService {
     private final DetailCommandeRepository detailCommandeRepository;
     private final ClientService clientService;
     private final MouvementStockService mouvementStockService;
+    private final MouvementSortieDetailRepository mouvementSortieDetailRepository;
+    private final LotProductionRepository lotProductionRepository;
+    private final StatutsLotProductionRepository statutsLotProductionRepository;
 
     public CommandeService(CommandeRepository commandeRepository, StatutCommandeRepository statutCommandeRepository,
-            DetailCommandeRepository detailCommandeRepository, ClientService clientService, MouvementStockService mouvementStockService) {
+            DetailCommandeRepository detailCommandeRepository, ClientService clientService, MouvementStockService mouvementStockService, MouvementSortieDetailRepository mouvementSortieDetailRepository, LotProductionRepository lotProductionRepository, StatutsLotProductionRepository statutsLotProductionRepository) {
         this.commandeRepository = commandeRepository;
         this.statutCommandeRepository = statutCommandeRepository;
         this.detailCommandeRepository = detailCommandeRepository;
         this.clientService = clientService;
         this.mouvementStockService = mouvementStockService;
+        this.mouvementSortieDetailRepository = mouvementSortieDetailRepository;
+        this.lotProductionRepository = lotProductionRepository;
+        this.statutsLotProductionRepository = statutsLotProductionRepository;
     }
 
     public void stockAvailable(DetailCommandeModel detail) throws StockUnavailableException {
@@ -103,6 +121,45 @@ public class CommandeService {
     }
 
     @Transactional
+    public void cancelCommande(Integer idCommande) {
+        StatutCommandeModel statutCommandeModel = new StatutCommandeModel();
+        CommandeModel commandeModel = commandeRepository.findById(idCommande).orElse(null);
+        statutCommandeModel.setCommande(commandeModel);
+        CommandeStatutModel commandeStatutModel = new CommandeStatutModel();
+        commandeStatutModel.setId(5);
+        statutCommandeModel.setStatut(commandeStatutModel);
+        statutCommandeModel.setDateStatutCommande(LocalDateTime.now());
+        statutCommandeRepository.save(statutCommandeModel);
+        List<MouvementSortieDetailModel> sortieDetails = mouvementSortieDetailRepository.findByMouvementSortie(commandeModel.getMouvementSortie());
+        for(MouvementSortieDetailModel detail : sortieDetails) {
+            LotProductionModel original = detail.getLotProduction();
+            LotProductionModel modified = new LotProductionModel();
+            modified.setDateEntreeLot(original.getDateEntreeLot());
+            modified.setDateFinReelle(original.getDateFinReelle());
+            modified.setProduit(original.getProduit());
+            modified.setQuantiteMatiereUtilisee(original.getQuantiteMatiereUtilisee());
+            modified.setQuantiteProduitPrevue(original.getQuantiteProduitPrevue());
+            modified.setQuantiteProduitReelle(original.getQuantiteProduitReelle());
+            modified.setRemarques("Depuis commande annulee");
+            modified.setReference("NO_REF");
+            modified.setTypeMatierePremiere(original.getTypeMatierePremiere());
+            LotProductionModel lotSaved = lotProductionRepository.save(modified);
+            StatutsLotProductionModel statutsLotProductionModel = new StatutsLotProductionModel();
+            LotStatutsModel lotStatutsModel = new LotStatutsModel();
+            lotStatutsModel.setId(2);
+            statutsLotProductionModel.setLotStatuts(lotStatutsModel);
+            statutsLotProductionModel.setLotProduction(lotSaved);
+            statutsLotProductionModel.setDateStatut(LocalDateTime.now());
+            statutsLotProductionRepository.save(statutsLotProductionModel);
+            EntreeStockDTO entreeStockDTO = new EntreeStockDTO();
+            entreeStockDTO.setDateEntree(LocalDate.now());
+            entreeStockDTO.setIdLot(lotSaved.getId());
+            entreeStockDTO.setQuantite(detail.getQuantite());
+            mouvementStockService.saveEntreeStock(entreeStockDTO);
+        }
+    }
+
+    @Transactional
     public void save(CommandeModel commande, List<DetailCommandeModel> details) {
         if (commande.getClient() != null && commande.getClient().getId() != null) {
             commande.setClient(clientService.findById(commande.getClient().getId()));
@@ -121,12 +178,37 @@ public class CommandeService {
 
     @Transactional
     public void saveAllDetails(List<DetailCommandeModel> details) {
+        for(DetailCommandeModel detailCommandeModel : details) {
+            SortieStockDTO sortieStockDTO = new SortieStockDTO();
+            sortieStockDTO.setDateSortie(LocalDate.now());
+            sortieStockDTO.setIdMotif(1);
+            sortieStockDTO.setIdProduit(detailCommandeModel.getProduit().getId());
+            sortieStockDTO.setQuantite(detailCommandeModel.getQuantite());
+            mouvementStockService.saveSortieStock(sortieStockDTO);
+        }
         detailCommandeRepository.saveAll(details);
     }
 
         @Transactional
     public void saveDetail(DetailCommandeModel detail) {
         detailCommandeRepository.save(detail);
+    }
+
+    public CommandeDto getCommandeInfo(Integer id) {
+        CommandeDto ret = new CommandeDto();
+        CommandeModel commande = findById(id);
+        List<DetailCommandeModel> details = findDetails(id);
+        ret.setClientNom(commande.getClient().getNom());
+        ret.setDateCommande(commande.getDateCommande());
+        ret.setDetails(details);
+        ret.setId(id);
+        Double amount = details.stream().mapToDouble(e -> e.findMontant()).sum();
+        ret.setMontant(amount);
+        ret.setReference(commande.getReference());
+        StatutCommandeModel lastStatut = statutCommandeRepository.getLastStatutOf(id);
+        ret.setIdCommandeStatuts(lastStatut.getStatut().getId());
+        ret.setStatutLibelle(lastStatut.getStatut().getLibelle());
+        return ret;
     }
 
     @Transactional
